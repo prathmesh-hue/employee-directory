641b002c1dbd40ff6643cc6d7e31cada25c22c8d
Prathmesh Bhilegaonkar <prathmeshbhilegaonkar@gmail.com>
2026-08-20 08:31:46 +0530

    Add static employee list component

--- employee-directory/src/app/app.ts
+++ employee-directory/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { EmployeeListComponent } from './employee-list/employee-list';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [EmployeeListComponent],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('employee-directory');
+  title = 'employee-directory';
 }
